99867cc0c1db6048281e31f9b56a21554d87dec5
KiokoMC <kioskomc@gmail.com>
2022-09-05 20:08:18 -0500

    Agregamos Condicionales al archivo

---
+++ programacion_basica.py
@@ -0,0 +1,15 @@
+#Variables
+
+enteros = 123
+booleans = True
+listas = ['', 21 ,0]
+tuplas = (78, 90, 89)
+diccionario = {
+    'llave' : 'valor',
+} 
+
+#Condicionales CAMBIOS EN VERDE Y MODIFICACIONES EN AZUL**** El git que me diga on toy
+if (100 > enteros):
+    print('Si es mayor')
+else:
+    print('No es mayor')
